bc325e9cf254120dc76e8dc8b2122bbeb8e10cce
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-05-16 02:03:23 +0300

    added secret ke and  PRODCONFIG class in config.py

--- config.py
+++ config.py
@@ -6,26 +6,21 @@ class Config(object):
      DEBUG = False
      TESTING = False
 
+     SECRET_KEY = ''
+     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 
 
 class ProductionConfig(Config):
     DEBUG = False
-
-
 class StagingConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-
-
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-
-
 class TestingConfig(Config):
     TESTING = True
-
-
 config_options = {
 'test':TestingConfig
 }
